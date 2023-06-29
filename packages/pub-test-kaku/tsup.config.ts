d6a1d230cb5f7b1583e16c2719364cbd8f36997d
gxz <guoxianzhe@agora.io>
2023-06-29 18:52:24 +0800

    fix: ci

--- packages/pub-test-kaku/tsup.config.ts
+++ packages/pub-test-kaku/tsup.config.ts
@@ -1,15 +1,57 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entryPoints: {
-    "pub-test-kaku": "src/index.ts",
+import pkg from "./package.json";
+
+const banner = `
+/**
+ * @license ${pkg.name}
+ * @version ${pkg.version}
+ *
+ * This source code is licensed under the MIT license.
+ */
+`;
+
+export default defineConfig([
+  {
+    entry: {
+      [pkg.name]: "src/index.ts",
+    },
+    banner: () => {
+      return {
+        js: banner,
+      };
+    },
+    format: ["cjs", "esm"],
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+    treeshake: true,
+    dts: true,
+    minify: false,
   },
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"],
-  splitting: false,
-  sourcemap: false,
-  clean: true,
-  treeshake: true,
-  dts: true,
-  minify: false,
-});
+  {
+    entry: {
+      [pkg.name]: "src/index.ts",
+    },
+    banner: () => {
+      return {
+        js: banner,
+      };
+    },
+    outExtension: () => {
+      return {
+        js: `.iife.${pkg.version}.js`,
+      };
+    },
+    format: ["iife"],
+    sourcemap: false,
+    splitting: false,
+    clean: true,
+    minify: true,
+    external: Object.keys(pkg.peerDependencies),
+    define: {
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    },
+    platform: "browser",
+  },
+]);
